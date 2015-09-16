b6aaa2961e79c57f7a10f2a3ac2c0364ede40783
figo645 <figo645@126.com>
2015-09-16 14:10:24 +0800

    dao crud

--- 3.implementtation/prototype_ws/dao/src/main/java/cn/edu/ken/main/ContactDao.java
+++ 3.implementtation/prototype_ws/dao/src/main/java/cn/edu/ken/main/ContactDao.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -12,6 +13,8 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import cn.edu.ken.dao.contactMapper;
 import cn.edu.ken.model.contact;
+import cn.edu.ken.model.contactExample;
+import cn.edu.ken.model.contactExample.Criteria;
 
 public class ContactDao {
 	private SqlSession session =null;
@@ -28,10 +31,60 @@ public class ContactDao {
         session.close();
 	}
 	
+	public void testDaoUpdate(){
+		session = initSession();  
+		vcontactMapper = session.getMapper(contactMapper.class);
+		contact c = new contact();
+		c.setId(1);
+		c.setAge(12);
+		//c.setContactname("name");
+		//vcontactMapper.insert(c);
+		
+		vcontactMapper.updateByPrimaryKeySelective(c);
+		session.commit();  
+        session.close();
+	}
+	
+	public void testDaoselectByid(){
+		session = initSession();  
+		vcontactMapper = session.getMapper(contactMapper.class);
+		contact c = new contact();
+		c.setId(1);
+		c.setAge(11);
+		c.setContactname("name");
+		//vcontactMapper.insert(c);
+		contact con = vcontactMapper.selectByPrimaryKey(1);
+		System.out.println(con.getContactname());
+		session.commit();  
+        session.close();
+	}
+	
+	public void testDaoselectByIdandName(){
+		session = initSession();  
+		vcontactMapper = session.getMapper(contactMapper.class);
+		contact c = new contact();
+		c.setId(1);
+		c.setAge(11);
+		c.setContactname("name");
+		//vcontactMapper.insert(c);
+		contactExample example = new contactExample();
+		contactExample.Criteria cri = example.createCriteria();
+		cri.andIdEqualTo(1);
+		cri.andContactnameLike("na%");
+		//List<contact> cons = vcontactMapper.selectByExample(example);
+		int count = vcontactMapper.countByExample(example);
+		System.out.println(count);
+		session.commit();  
+        session.close();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ContactDao dao = new ContactDao();
-		dao.testDaoInsert();
+		//dao.testDaoInsert();
+		//dao.testDaoUpdate();
+		//dao.testDaoselectByid();
+		dao.testDaoselectByIdandName();
 	}	
 	
 	private SqlSession initSession(){
