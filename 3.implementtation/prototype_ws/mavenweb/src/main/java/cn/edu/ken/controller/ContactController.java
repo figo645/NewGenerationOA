2a3aea37f759511d2c35bd096cb070ccc5ba5f3e
figo645 <figo645@126.com>
2015-09-23 10:38:04 +0800

    sqlprovider test

--- 3.implementtation/prototype_ws/mavenweb/src/main/java/cn/edu/ken/controller/ContactController.java
+++ 3.implementtation/prototype_ws/mavenweb/src/main/java/cn/edu/ken/controller/ContactController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.ibatis.annotations.SelectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
